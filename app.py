862b7203ef7edc5471ff2eea5263eb04efe79ebd
madhav06 <mnandan06@gmail.com>
2021-07-26 19:50:52 +0530

    Location endpoints GET, POST, PATCH, DELETE requests

--- app.py
+++ app.py
@@ -101,10 +101,100 @@ class Users(Resource):
 
 class Locations(Resource):
     # methods go here
-    pass
+    def get(self):
+        data = pd.read_csv('locations.csv')  # read local csv
+        return {'data': data.to_dict()}, 200  # return data dict and 200 OK
 
-api.add_resource(Users, '/users')  # '/users' is our entry point for Users
-api.add_resource(Locations, '/locations')  # and '/locations' is our entry point for Locations
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize parser
+        parser.add_argument('locationId', required=True, type=int)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('rating', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('locations.csv')
+
+        # check if location already exists
+        if args['locationId'] in list(data['locationId']):
+            # if locationId already exists, return 401 unauthorized
+            return {
+                'message': f" '{args['locationId']}' already exists."
+            }, 409
+        else:
+            # otherwise, we can add the new location record
+            # create new dataframe containing new values
+            new_data = pd.DataFrame({
+                'locationId': [args['locationId']],
+                'name': [args['name']],
+                'rating': [args['rating']]
+            })
+            # add the newly provided values
+            data = data.append(new_data, ignore_index=True)
+            data.to_csv('locations.csv', index=False)  # save back to csv
+            return {'data': data.to_dict()}, 200  # return data with 200 OK
+
+    
+    def patch(self):
+        parser = reqparse.RequestParser()  # initialize parser
+        parser.add_argument('locationId', required=True, type=int)  # add args
+        parser.add_argument('name', store_missing=False)
+        parser.add_argument('rating', store_missing=False)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('locations.csv')
+
+        # check that the location exists
+        if args['locationId'] in list(data['locationId']):
+            # if it exists, we can updateit, first we get the user row
+            user_data = data[data['locationId'] == args['locationId']]
+
+            # if name has been provided, we update name
+            if 'name' in args:
+                user_data['name'] = args['name']
+            # if rating has been provided, we update rating
+            if 'rating' in args:
+                user_data['rating'] = args['rating']
+
+            # update data
+            data[data['locationId'] == args['locationId']] = user_data
+            # save updated data
+            data.to_csv('locations.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise return 404 not found
+            return {
+                'message': f" '{args['locationId']}' location doesn't exists."
+            }, 404
+
+    def delete(self):
+        parser = reqparse.RequestParser()  # initialize parser
+        parser.add_argument('locationId', required=True, type=int)  # add locationId arg
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv('locations.csv')
+
+        # check that location exists
+        if args['locationId'] in list(data['locationId']):
+            # if it exists, we delete it
+            data = data[data['locationId'] != args['locationId']]
+            # save the data
+            data.to_csv('locations.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise we return 404 not found
+            return {
+                'message': f" '{args['locationId']}' location doesn't exist. "
+            }
+
+api.add_resource(Users, '/users')  # '/users' is our endpoints for Users
+api.add_resource(Locations, '/locations')  # and '/locations' is our endpoints for Locations
 
 if __name__ == '__main__':
     app.run()  # run our flask app
